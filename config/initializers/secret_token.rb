56828149eafc826eab3ef21cf102bbefb959ee78
Joshua Kappers <josh@jkappers.me>
2013-12-11 19:12:46 -0600

    Generate rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Versus::Application.config.secret_key_base = '16039666c8b24174a381e4f559087ee2d671282118daf998199d5c7b81d720cbfe6f510058913a6d5b87e289129fc7455e3349286ccfd15f82c23ce88885e413'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Versus::Application.config.session_store :cookie_store, key: '_versus_session'
