1c11c00df30cc8e8f47a997ef75dce321bcb596d
Mike Mayo <gotmayonase@gmail.com>
2013-12-15 23:13:09 -0600

    adding production urban airship configs

--- config/initializers/urbanairship.rb
+++ config/initializers/urbanairship.rb
@@ -1,5 +1,12 @@
-Urbanairship.application_key = 'te-JTfoxRL-LmDGY9avL_A'
-Urbanairship.application_secret = 'ZidP5bpWRxy5Om3C0JORAQ'
-Urbanairship.master_secret = 'Vw8p759xSiq4peqG-sZ-Gg'
+if Rails.env == 'development'
+  Urbanairship.application_key = 'te-JTfoxRL-LmDGY9avL_A'
+  Urbanairship.application_secret = 'ZidP5bpWRxy5Om3C0JORAQ'
+  Urbanairship.master_secret = 'Vw8p759xSiq4peqG-sZ-Gg'
+else
+  Urbanairship.application_key = 'DL1r-6wVRsK3EuAtzhCjEA'
+  Urbanairship.application_secret = 'OgeUXul0Qu-P2ynN8UBgCA'
+  Urbanairship.master_secret = 'ATtvetKWS5GCkMnaArSVfQ'
+end
+
 Urbanairship.logger = Rails.logger
 Urbanairship.request_timeout = 5 # default
