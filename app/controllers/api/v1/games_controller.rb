af0ae2371c25d2e860f315047e6f49fa0cc6c574
Mike Mayo <gotmayonase@gmail.com>
2013-12-17 00:23:04 -0600

    disallow wins against yourself

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,12 +5,17 @@ module Api::V1
 		end
 
     def create
-      if Game.where(winner_id: current_user.id, loser_id: game_params[:loser_id]).where("created_at > ?", Time.now - 5.minutes).exists?
-        render status: :forbidden, :json => { errors: 'You just posted a win against this player.' }
+      loser_id = game_params[:loser_id]
+      if current_user.id == loser_id.to_i
+        render status: :forbidden, :json => { errors: 'You just tried to register a win against yourself. You\'re not too bright, are you?' }
+        return
+      end
+      if Game.where(winner_id: current_user.id, loser_id: loser_id).where("created_at > ?", Time.now - 5.minutes).exists?
+        render status: :forbidden, :json => { errors: 'You just posted a win against this player. Chill!' }
         return
       end
       @game = Game.new({
-        loser_id:  game_params[:loser_id],
+        loser_id:  loser_id,
         winner: current_user
       })
       
